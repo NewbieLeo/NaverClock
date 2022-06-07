4011b2313c7bb56a5dd9fa9929ca86efd0c45a01
newbieleo <jogangmin07@gmail.com>
2022-06-07 19:25:01 +0900

    1.1

---
+++ app.js
@@ -0,0 +1,27 @@
+setInterval(() => {
+    let now = new Date();
+    var hr = (""+now.getHours()%12).padStart(2, "0").replace("00", "12");
+    var min = (""+now.getMinutes()).padStart(2, "0");
+    var sec = (""+now.getSeconds()).padStart(2, "0");
+    var apm = (now.getHours() < 12) ? "오전" : "오후";
+    
+    document.getElementById("hr").innerHTML = hr;
+    document.getElementById("min").innerHTML = min;
+    document.getElementById("sec").innerHTML = sec;
+    document.getElementById("apm").innerHTML = apm;
+
+    document.title = `현재 시각: ${apm} ${hr}:${min}:${sec}`
+
+    if (sec >= 50) document.getElementById("sec").style.color = "red";
+    else document.getElementById("sec").style.color = "black";
+    
+    if (now.getMilliseconds() >= 500) document.querySelectorAll(".colon").forEach(item => item.style.color = "rgba(0, 0, 0, 0.7)");
+    else document.querySelectorAll(".colon").forEach(item => item.style.color = "rgba(0, 0, 0, 0.3)");
+
+}, 25);
+
+const hrButton = document.querySelector("input[id=hr24]")
+hrButton.onclick = () => {
+    if (hrButton.checked == "true") document.querySelector(".box").classList.remove("hr24");
+    else document.querySelector(".box").classList.add("hr24");
+}
